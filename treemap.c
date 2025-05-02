480490cf3489b28cc9eadfe93b748c5081bd0976
Your Name <you@example.com>
2025-05-02 18:56:07 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -176,6 +176,9 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
+    if(tree->root == NULL) return NULL;
+    tree->current = tree->root;
+    
     while(tree->current->left != NULL)
     {
         tree->current = tree->current->left;
